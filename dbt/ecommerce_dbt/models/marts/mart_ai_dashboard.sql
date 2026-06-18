57a74109e51edb19fb47550788e433aaf58f93ad
VIVEK TIWARI <vivekazurelearning1@gmail.com>
2026-06-18 05:35:46 +0000

    salesaalytics

--- dbt/ecommerce_dbt/models/marts/mart_ai_dashboard.sql
+++ dbt/ecommerce_dbt/models/marts/mart_ai_dashboard.sql
@@ -1,6 +1,5 @@
 {{ config(materialized='table') }}
 
-SELECT *
-FROM {{ source('ecommerce','SALES_ANALYTICS') }}
+SELECT * FROM {{ source('ecommerce','SALES_ANALYTICS') }}
 
 ---dbt run --select mart_ai_dashboard
